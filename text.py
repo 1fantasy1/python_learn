3b5c7cc16c092bfb7a415d55f090f6f452a28e18
1fantasy1 <127208503+1fantasy1@users.noreply.github.com>
2024-01-28 13:28:21 +0800

    1

--- text.py
+++ text.py
@@ -571,29 +571,29 @@
 # print(stu_1.native_place)
 # print(stu_1.age)
 
-# 定义一个带成员方法的类
-class Student():
-    name = None # 学生姓名
-
-    # self 必须写
-    def say_hi(self):
-        print(f'大家好，我是{self.name},大家多多关照')
-
-    def say_hi2(self,msg):
-        print(f"大家好，我是{self.name},{msg}")
-
-stu = Student()
-stu.name = "小明"
-stu.say_hi()
-
-stu2 = Student()
-stu2.name = "小王"
-stu2.say_hi()
-
-stu = Student()
-stu.name = "小明"
-stu.say_hi2("哎呦，不错哦")
-
-stu2 = Student()
-stu2.name = "小王"
-stu2.say_hi2("小伙子，我看好你哟")
+# # 定义一个带成员方法的类
+# class Student():
+#     name = None # 学生姓名
+#
+#     # self 必须写
+#     def say_hi(self):
+#         print(f'大家好，我是{self.name},大家多多关照')
+#
+#     def say_hi2(self,msg):
+#         print(f"大家好，我是{self.name},{msg}")
+#
+# stu = Student()
+# stu.name = "小明"
+# stu.say_hi()
+#
+# stu2 = Student()
+# stu2.name = "小王"
+# stu2.say_hi()
+#
+# stu = Student()
+# stu.name = "小明"
+# stu.say_hi2("哎呦，不错哦")
+#
+# stu2 = Student()
+# stu2.name = "小王"
+# stu2.say_hi2("小伙子，我看好你哟")
