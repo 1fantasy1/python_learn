c50da0db1dcef48899113f3b5978353c257ea840
1fantasy1 <127208503+1fantasy1@users.noreply.github.com>
2024-01-31 13:34:18 +0800

    1

--- python爬虫.py
+++ python爬虫.py
@@ -1,11 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
-head = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0"}
-r = requests.get('https://movie.douban.com/top250',headers=head)
-soup = BeautifulSoup(r.text,'html.parser')
-all_name = soup.findAll('span', attrs={'class':'title'})
-for i in all_name:
-    print(i.string.strip(" / "))
-
+i = 1
+for start_num in range(0,250,25):
+    head = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0"}
+    r = requests.get(f'https://movie.douban.com/top250?start={start_num}',headers=head)
+    soup = BeautifulSoup(r.text,'html.parser')
+    all_name = soup.findAll('span', attrs={'class':'title'})
+    for name in all_name:
+        title = name.string
+        if "/" not in title:
+            print(f"{i}.{title}")
+            i += 1
 # print(r.status_code)
 # print(r.text)
