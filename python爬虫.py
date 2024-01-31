749b8d77d47b0e19d3c39280fd03513cd0e5649f
1fantasy1 <127208503+1fantasy1@users.noreply.github.com>
2024-01-31 12:47:03 +0800

    1

--- python爬虫.py
+++ python爬虫.py
@@ -1,5 +1,11 @@
 import requests
+from bs4 import BeautifulSoup
 head = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0"}
 r = requests.get('https://movie.douban.com/top250',headers=head)
-print(r.status_code)
-print(r.text)
+soup = BeautifulSoup(r.text,'html.parser')
+all_name = soup.findAll('span', attrs={'class':'title'})
+for i in all_name:
+    print(i.string.strip(" / "))
+
+# print(r.status_code)
+# print(r.text)
